6454c203bd0d44f436ea85a2c680cde4d8b8e0d2
Ram Krishna <kukutlak@outlook.com>
2016-06-10 17:40:42 -0500

    Child table added

--- 1P_Of_Life/WebContent/js/app.js
+++ 1P_Of_Life/WebContent/js/app.js
@@ -1,14 +1,16 @@
-var app = angular.module('1PHomeApp',['ngRoute']);
-app.config(['$routeProvider', function($routeProvider){
+var myApp = angular.module('1PHomeApp',['ngRoute']);
+myApp.config(['$routeProvider', function($routeProvider){
     
     $routeProvider
+        .when('\ChildInfo',{templateUrl:'ChildMaster.html'})
         .when('\left',{templateUrl:'leftPanel.html'})
         .when('\right', {templateUrl:'rightPanel.html'})
+        
         .otherwise ({redirectTo:'/'});
     
 }]);
 
-app.controller('1PHomeController',function($scope,$timeout){
+myApp.controller('1PHomeController',function($scope,$timeout){
     
     $scope.x = 'Stu1';
     $scope.y= 'Stu2';
