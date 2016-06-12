2382392f69102cf30eef1581e667ffbfac4f2e17
Ram Krishna <kukutlak@outlook.com>
2016-06-12 02:24:51 -0500

    Menu Bar added

--- 1P_Of_Life/WebContent/js/app.js
+++ 1P_Of_Life/WebContent/js/app.js
@@ -1,12 +1,12 @@
-var myApp = angular.module('1PHomeApp',['ngRoute']);
+var myApp = angular.module('1PHomeApp',['ngRoute','ngMaterial', 'ngMessages', 'material.svgAssetsCache']);
 myApp.config(['$routeProvider', function($routeProvider){
     
-    $routeProvider
-        .when('\ChildInfo',{templateUrl:'ChildMaster.html'})
-        .when('\left',{templateUrl:'leftPanel.html'})
-        .when('\right', {templateUrl:'rightPanel.html'})
-        
-        .otherwise ({redirectTo:'/'});
+//    $routeProvider
+//        .when('/childInfo',{templateUrl:'ChildMaster.html', controller='MasterDetailCtrl'})
+//        .when('/left',{templateUrl:'leftPanel.html'})
+//        .when('/right', {templateUrl:'rightPanel.html'})
+//        
+//        .otherwise ({redirectTo:'/'});
     
 }]);
 
@@ -18,5 +18,47 @@ myApp.controller('1PHomeController',function($scope,$timeout){
     
 })
 
+angular
+  .module('1PHomeApp')
+  .config(function($mdIconProvider) {
+    $mdIconProvider
+      .defaultIconSet('img/icons/sets/core-icons.svg', 24);
+  })
+  .filter('keyboardShortcut', function($window) {
+    return function(str) {
+      if (!str) return;
+      var keys = str.split('-');
+      var isOSX = /Mac OS X/.test($window.navigator.userAgent);
 
+      var seperator = (!isOSX || keys.length > 2) ? '+' : '';
+
+      var abbreviations = {
+        M: isOSX ? '' : 'Ctrl',
+        A: isOSX ? 'Option' : 'Alt',
+        S: 'Shift'
+      };
+
+      return keys.map(function(key, index) {
+        var last = index == keys.length - 1;
+        return last ? key : abbreviations[key];
+      }).join(seperator);
+    };
+  })
+  .controller('DemoBasicCtrl', function DemoCtrl($mdDialog) {
+    this.settings = {
+      printLayout: true,
+      showRuler: true,
+      showSpellingSuggestions: true,
+      presentationMode: 'edit'
+    };
+
+    this.sampleAction = function(name, ev) {
+      $mdDialog.show($mdDialog.alert()
+        .title(name)
+        .textContent('You triggered the "' + name + '" action')
+        .ok('Great')
+        .targetEvent(ev)
+      );
+    };
+  });
 
